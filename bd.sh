64516604f66cab2b89522e72bc696fd4218484a0
Daria Usova <usovadasha@myttk.ru>
2023-01-10 00:09:23 +0700

    Make bd with dockerfile

---
+++ bd.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+clickhouse-client --query "CREATE TABLE recipes(title String, ingredients Array(String), directions Array(String), link String, source LowCardinality(String), NER Array(String)) ENGINE = MergeTree ORDER BY title;"
+clickhouse-client --query "INSERT INTO recipes SELECT title, JSONExtract(ingredients, 'Array(String)'), JSONExtract(directions, 'Array(String)'), link, source, JSONExtract(NER, 'Array(String)') FROM input('num UInt32, title String, ingredients String, directions String, link String, source LowCardinality(String), NER String') FORMAT CSVWithNames" --input_format_with_names_use_header 0 --format_csv_allow_single_quote 0 --input_format_allow_errors_num 10 < /etc/clickhouse-server/full_dataset.csv
