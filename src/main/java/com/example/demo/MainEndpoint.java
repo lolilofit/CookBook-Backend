f01f83518513be06c06827ab094064dea42b2999
Daria Usova <usovadasha@myttk.ru>
2023-01-12 21:41:21 +0700

    Fix

--- src/main/java/com/example/demo/MainEndpoint.java
+++ src/main/java/com/example/demo/MainEndpoint.java
@@ -8,6 +8,7 @@ import com.example.demo.model.Recipes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class MainEndpoint {
     private IRecipePicker recipePicker;
 
     @RequestMapping(value = "/main", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Recipes> getRecipes(byte[] image) throws SQLException {
+    public ResponseEntity<Recipes> getRecipes(@RequestBody byte[] image) throws SQLException {
         System.out.println("See request...");
         ExistingIngredients existingIngredients = recognitionHelper.recognizeIngredients(image);
         Recipes recipes = recipePicker.pickRecipes(existingIngredients, maxRecipesNumber);
